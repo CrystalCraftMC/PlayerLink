e6e8086751dd81020438d597952ba23d7ccebffc
Justin Flory <jflory7@gmail.com>
2013-07-22 15:49:45 -0400

    Updated PlayerLinkCmd class

--- src/com/crystalcraftmc/playerlink/PlayerLinkCmd.java
+++ src/com/crystalcraftmc/playerlink/PlayerLinkCmd.java
@@ -53,7 +53,16 @@ public class PlayerLinkCmd implements CommandExecutor
 	        	// If the sender of the command has this permission...
 	    		if(p.hasPermission("playerlink.help"))
 	    		{
-	    			plugin.getDescription().getCommands();
+	    			p.sendMessage(ChatColor.GOLD + "=-=-=-=-> " + ChatColor.YELLOW + "PlayerLink Commands" + ChatColor.GOLD + " <-=-=-=-=");
+	        		p.sendMessage(ChatColor.GOLD + "[" + ChatColor.YELLOW + "PL" + ChatColor.GOLD + "] " + ChatColor.GOLD + "Version: " + ChatColor.RED + "1.1");
+	        		p.sendMessage(ChatColor.GOLD + "[" + ChatColor.YELLOW + "PL" + ChatColor.GOLD + "] " + ChatColor.RED + "/PlayerLink: " + ChatColor.GOLD + "Shows commands in the plugin.");
+	        		p.sendMessage(ChatColor.GOLD + "[" + ChatColor.YELLOW + "PL" + ChatColor.GOLD + "] " + ChatColor.RED + "/website: " + ChatColor.GOLD + "Displays a link to the server website.");
+	        		p.sendMessage(ChatColor.GOLD + "[" + ChatColor.YELLOW + "PL" + ChatColor.GOLD + "] " + ChatColor.RED + "/vote: " + ChatColor.GOLD + "Displays a link to a voting website for this server.");
+	        		p.sendMessage(ChatColor.GOLD + "[" + ChatColor.YELLOW + "PL" + ChatColor.GOLD + "] " + ChatColor.RED + "/facebook: " + ChatColor.GOLD + "Displays a link to the server Facebook page.");
+	        		p.sendMessage(ChatColor.GOLD + "[" + ChatColor.YELLOW + "PL" + ChatColor.GOLD + "] " + ChatColor.RED + "/twitter: " + ChatColor.GOLD + "Displays a link to the server Twitter page.");
+	        		p.sendMessage(ChatColor.GOLD + "[" + ChatColor.YELLOW + "PL" + ChatColor.GOLD + "] " + ChatColor.RED + "/youtube: " + ChatColor.GOLD + "Displays a link to the server YouTube page.");
+	        		p.sendMessage(ChatColor.GOLD + "[" + ChatColor.YELLOW + "PL" + ChatColor.GOLD + "] " + ChatColor.RED + "/link: " + ChatColor.GOLD + "Displays a link to whatever an admin has set.");
+	        		p.sendMessage(ChatColor.GOLD + "=-=-=-=-> " + ChatColor.YELLOW + "PlayerLink Commands" + ChatColor.GOLD + " <-=-=-=-=");
 	    		}
 	    		
 	    		// Otherwise, if the sender of the command doesn't have the permission...
